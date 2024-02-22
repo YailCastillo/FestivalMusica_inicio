447794a4b8c84277f4be9bbc335b6e6f8bf77616
YailCastillo <yailcastillomtz@gmail.com>
2024-02-22 11:43:07 -0600

    cache, imagemin, webp & avif added

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,14 @@
-const {src, dest, watch} = require("gulp");
+const {src, dest, watch, parallel} = require("gulp");
+
+//CSS
 const sass = require("gulp-sass")(require('sass'));
 const plumber = require('gulp-plumber');
+
+//Imagenes
+const cache = require('gulp-cache');
+const imagemin = require("gulp-imagemin");
+const webp = require('gulp-webp');
+const avif = require('gulp-avif');
 
 function css(done) {
     //Identificar el archivo de sass
@@ -12,6 +20,42 @@ function css(done) {
     done();
 }
 
+function images(done) {
+    const options = {
+        optimizationLevel: 3
+    };
+
+    src('src/img/**/*.{png,jpg}')
+        .pipe(cache(imagemin(options)))
+        .pipe(dest('build/img'));
+    
+    done();
+}
+
+function versionWebp(done) {
+    const options = {
+        quality: 50
+    };
+
+    src('src/img/**/*.{png,jpg}')
+        .pipe(webp(options))
+        .pipe(dest('build/img'));
+    
+    done();
+}
+
+function versionAvif(done) {
+    const options = {
+        quality: 50
+    };
+
+    src('src/img/**/*.{png,jpg}')
+        .pipe(avif(options))
+        .pipe(dest('build/img'));
+    
+    done();
+}
+
 function dev(done) {
     watch("src/scss/**/*.scss", css)
 
@@ -19,4 +63,7 @@ function dev(done) {
 }
 
 exports.css = css;
-exports.dev = dev;
+exports.images = images;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+exports.dev = parallel(images, versionWebp, versionAvif, dev);
