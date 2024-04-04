87ab249ab1fa597fbffd017a6c2b5c7b38fc8cb5
YailCastillo <yailcastillomtz@gmail.com>
2024-04-04 11:40:00 -0600

    Sourcemaps & terser js added

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ const plumber = require('gulp-plumber');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
 const postcss = require('gulp-postcss');
+const sourcemaps = require('gulp-sourcemaps');
 
 //Imagenes
 const cache = require('gulp-cache');
@@ -13,12 +14,17 @@ const imagemin = require("gulp-imagemin");
 const webp = require('gulp-webp');
 const avif = require('gulp-avif');
 
+//Javascript
+const terser = require('gulp-terser-js');
+
 function css(done) {
     //Identificar el archivo de sass
     src("src/scss/**/*.scss") //Identificar el archivo sass
+        .pipe(sourcemaps.init())
         .pipe(plumber())
         .pipe(sass()) //compilarlo
         .pipe(postcss([autoprefixer(), cssnano()]))
+        .pipe(sourcemaps.write('.'))
         .pipe(dest("build/css")); //Guardar en disco duro
     
     done();
@@ -62,6 +68,9 @@ function versionAvif(done) {
 
 function javascript(done) {
     src('src/js/**/*.js')
+        .pipe(sourcemaps.init())
+        .pipe(terser())
+        .pipe(sourcemaps.write('.'))
         .pipe(dest('build/js'));
 
     done();
